37e9144dfa636d33380f58c3c3e104e94255fad1
Leonardo leal <74279418+DukeD-27@users.noreply.github.com>
2021-03-31 11:18:45 -0300

    Delete cadastroDados.php

--- cadastroDados.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	$vnome = $_POST["name"];
-	$vemail= $_POST["email"];
-	$vsenha = $_POST["password"];
-
-	print "Nome: ".$vnome."<br/>E-mail: ".$vemail."<br/>Senha: ".$vsenha."<br/>";
-
-?>
